64b1e46924072ccb53b9d0f1d89907c5ca02d7db
zhixu li <zhixuli0406@gmail.com>
2026-03-30 06:46:58 +0800

    chore: bump homebrew formula to v0.9.1

--- HomebrewFormula/duduclaw.rb
+++ HomebrewFormula/duduclaw.rb
@@ -1,8 +1,8 @@
 class Duduclaw < Formula
   desc "Multi-Agent AI Assistant Platform"
   homepage "https://github.com/zhixuli0406/DuDuClaw"
-  url "https://github.com/zhixuli0406/DuDuClaw.git", tag: "v0.9.0"
-  version "0.9.0"
+  url "https://github.com/zhixuli0406/DuDuClaw.git", tag: "v0.9.1"
+  version "0.9.1"
   license "Elastic-2.0"
 
   head "https://github.com/zhixuli0406/DuDuClaw.git", branch: "main"
