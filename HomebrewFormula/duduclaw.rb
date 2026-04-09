a8b9723575e34ada5ce156b3a47343ecfae9abac
zhixu li <zhixuli0406@gmail.com>
2026-04-09 19:58:01 +0800

    fix: correct Homebrew formula version to 1.3.7

--- HomebrewFormula/duduclaw.rb
+++ HomebrewFormula/duduclaw.rb
@@ -2,7 +2,7 @@ class Duduclaw < Formula
   desc "Multi-Agent AI Assistant Platform"
   homepage "https://github.com/zhixuli0406/DuDuClaw"
   url "https://github.com/zhixuli0406/DuDuClaw.git", tag: "v1.3.7"
-  version "1.3.6"
+  version "1.3.7"
   license "Elastic-2.0"
 
   head "https://github.com/zhixuli0406/DuDuClaw.git", branch: "main"
